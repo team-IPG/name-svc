961bcf483463ff55d751e0fdb1ed52b3fb0a33fd
john <john.a.glynn@gmail.com>
2022-05-13 16:32:42 -0500

    allow CORS from all domains

--- src/main/java/org/ipg/namesvc/NameSvcApplication.java
+++ src/main/java/org/ipg/namesvc/NameSvcApplication.java
@@ -2,12 +2,27 @@ package org.ipg.namesvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class NameSvcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NameSvcApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				// Allow calls from all origins because YOLO
+				registry.addMapping("/**")
+						.allowedOrigins("*");
+			}
+		};
 	}
 
 }
