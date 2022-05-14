a0047c6d801882c8442c796644153a23337b27b3
john <john.a.glynn@gmail.com>
2022-05-13 22:25:47 -0500

    add docs reference

--- src/main/java/org/ipg/namesvc/repo/EmployeeRepository.java
+++ src/main/java/org/ipg/namesvc/repo/EmployeeRepository.java
@@ -8,8 +8,10 @@ import java.util.List;
 @Repository
 public interface EmployeeRepository extends YsqlRepository<Employee, String> {
 
+    // See: https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
+
     List<Employee> findByIdStartingWithIgnoreCase(String id);
     List<Employee> findByLastNameStartingWithIgnoreCase(String lastName);
     List<Employee> findByFirstNameStartingWithIgnoreCase(String firstName);
-    
+
 }
